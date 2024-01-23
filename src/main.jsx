a58f8a7bf78a5daaea7781859a701a205bbd96d6
unknown <roee1454@gmail.com>
2024-01-23 23:15:08 +0200

    sadasd

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import "react-toastify/dist/ReactToastify.min.css"
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { BrowserRouter } from "react-router-dom";
+import "react-toastify/dist/ReactToastify.min.css";
+import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.Fragment>
-    <App />
-  </React.Fragment>,
-)
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.Fragment>
+);
--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,30 +1,28 @@
-import React, { useContext } from 'react'
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
-import Layout from './Layout'
-import DroneTagging from '../components/DroneTagging'
-import UploadFile from '../components/UploadFile'
-import Excel from '../components/Excel'
-import { UserContext } from '../context/userContext'
-import SignIn from '../components/SignIn'
+import React, { useContext } from "react";
+import {  Routes, Route } from "react-router-dom";
+import Layout from "./Layout";
+import DroneTagging from "../components/DroneTagging";
+import UploadFile from "../components/UploadFile";
+import Excel from "../components/Excel";
+import { UserContext } from "../context/userContext";
+import SignIn from "../components/SignIn";
 
 const AppRoutes = () => {
-    const { userData } = useContext(UserContext);
-    if (!userData) {
-        return <SignIn />; 
-    }
-    return (
-        <div>
-            <Router>
-                <Routes>
-                    <Route path='/' element={<Layout />}  >
-                        <Route index element={<DroneTagging />} />
-                        <Route path='upload_file' element={<UploadFile/>} />
-                        <Route path='excel' element={<Excel/>} />
-                    </Route>
-                </Routes>
-            </Router>
-        </div>
-    )
-}
+  const { userData } = useContext(UserContext);
+  if (!userData) {
+    return <SignIn />;
+  }
+  return (
+    <div>
+      <Layout />
+        <Routes>
+          <Route index element={<DroneTagging />} />
+          <Route path="upload_file" element={<UploadFile />} />
+          <Route path="excel" element={<Excel />} />
+        </Routes>
 
-export default AppRoutes
+    </div>
+  );
+};
+
+export default AppRoutes;
